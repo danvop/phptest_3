73df265facc830a9ec884699d4db9547ad6b7c4f
danvop <danvop@gmail.com>
2015-06-05 15:58:01 +0300

    constructor & desctuctor added

--- oop/users.php
+++ oop/users.php
@@ -2,6 +2,14 @@
 class User{
   public $name, $login, $password;
 
+  function __construct($name, $login, $password){
+    $this->name = $name;
+    $this->login = $login;
+    $this->password = $password;
+  }
+  function __destruct(){
+    echo "$this->name [$this->login] deleted<br>";
+  }
   function showInfo(){
     echo "$this->name : $this->login
     : $this->password";
@@ -9,20 +17,9 @@ class User{
   }
 }
 
-$user1 = new User();
-$user2 = new User();
-$user3 = new User();
-$user1->name = 'Nikolay';
-$user1->login = 'Kolyan1';
-$user1->password = '123\@AF';
-
-$user2->name = 'Boris';
-$user2->login = 'Boris1953';
-$user2->password = '16041953';
-
-$user3->name = 'Vladimir';
-$user3->login = 'Volodya';
-$user3->password = 'Valdemar123';
+$user1 = new User('Nikolay','Kolyan1','123\@AF');
+$user2 = new User('Boris','Boris1953','16041953');
+$user3 = new User('Vladimir','Volodya','Valdemar123');
 
 $user1->showInfo();
 $user2->showInfo();
