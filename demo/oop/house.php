efecb0e5be9528e5449d744a648abe2811895c6d
danvop <danvop@gmail.com>
2015-06-08 14:46:41 +0300

    abstract class fixed

--- demo/oop/house.php
+++ demo/oop/house.php
@@ -1,5 +1,51 @@
 <?php
+
+abstract class HouseAbstract{
+  public $model = "";
+  public $square;
+  public $floors;
+  
+  function __construct($model,$square = 0,$floors = 1){
+    if(!$model)
+      throw new Exception('Ошибка!Укажите модель!');
+    $this->model = $model;
+    $this->square = $square;
+    $this->floors = $floors;
+  }
+  function startProject(){
+      echo "Start. Model: {$this->model}\n";
+  }
+  function stopProject(){
+    echo "Stop. Model: {$this->model}\n\n";
+  }
+  // Абстрактный метод
+  abstract function build();
+}
+
 // Создание супер-класса
+
+class SimpleHouse extends HouseAbstract{
+  // Свойства абстрактного класса +
+  public $color = "none";
+  // Обязательная реализация абстрактного метода
+  function build(){
+    echo "Build. House: {$this->square}x{$this->floors}\n";
+  }
+  // Свой метод
+  function paint(){
+    echo "Paint. Color: {$this->color}\n";
+  }  
+}
+
+// Создание простого дома
+$simple = new SimpleHouse("A-100-123", 120, 2);
+$simple->color = "red";
+$simple->startProject();
+$simple->build();
+$simple->paint();
+$simple->stopProject();
+
+/*// Создание супер-класса
 class SimpleHouse{
   public $model = "";
   public $square = 0;
@@ -82,4 +128,4 @@ $super_fabric->color = "black";
 $super_fabric->startProject();
 $super_fabric->build();
 $super_fabric->paint();
-$super_fabric->stopProject();
+$super_fabric->stopProject();*/
