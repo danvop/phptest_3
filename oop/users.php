6d74481a0cd260a7e021f2a61fc8cc994af8683b
danvop <danvop@gmail.com>
2015-06-09 15:16:59 +0300

    class files added, class autoload fixed

--- oop/users.php
+++ oop/users.php
@@ -1,56 +1,7 @@
 <?php
-interface ISuperUser{
-  function getInfo();
+function __autoload($name){
+  require "$name.class.php";
 }
-
-abstract class UserAbstract{
-  abstract function showInfo();
-}
-
-class User extends UserAbstract{
-  public $name, $login, $password;
-  public static $countUser = 0;
-
-  function __construct($name, $login, $password){
-    $this->name = $name;
-    $this->login = $login;
-    $this->password = $password;
-    ++self::$countUser;
-  }
-  function __destruct(){
-    echo "$this->name [$this->login] deleted<br>";
-  }
-  function showInfo(){
-    echo "$this->name : $this->login
-    : $this->password";
-    echo "<br>";
-  }
-}
-
-class SuperUser extends User implements ISuperUser{
-  public $role;
-  public static $countSuperUser;
-  function __construct($n, $l, $p, $r){
-    parent::__construct($n, $l, $p);
-    $this->role = $r;
-    ++self::$countSuperUser;
-  }
-  function showInfo(){
-    parent::showInfo();
-    echo "Role: {$this->role}\n";
-  }
-//getInfo returns assoc array with elem_name = prop and values = prop_values
-  function getInfo(){
-    $getInfo;
-    foreach($this as $name => $value){
-      $getInfo = $this;
-    }
-    return $getInfo;
-  }
-}
-
-
-
 $user1 = new User('Nikolay','Kolyan1','123\@AF');
 $user1->showInfo();
 
