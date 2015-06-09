17e6a57f83206642939e3a7c89ee4eff3eee83fc
danvop <danvop@gmail.com>
2015-06-09 14:07:12 +0300

    db.php - examples: abstract interface implement

---
+++ demo/oop/db.php
@@ -0,0 +1,18 @@
+<?php
+
+interface Db{
+  function connect();
+  function close();
+  //.....
+}
+
+interface X{
+  function connect();
+}
+
+class A implements Db, X{
+  function connect(){}
+  function close(){}
+}
+
+$a = new A();
