9db7cfd0a9717e7ea60e801a7889bfa5956c6329
danvop <danvop@gmail.com>
2015-07-03 00:00:39 +1000

    func saveNews fixed

--- news/NewsDB.class.php
+++ news/NewsDB.class.php
@@ -37,8 +37,23 @@ class NewsDB implements INewsDB{
   function __destruct(){
     unset($this->_db);
   }
-  function saveNews($title, $category, $description, $source){}
+  function saveNews($title, $category, $description, $source){
+    $dt = time();
+    $sql = "INSERT INTO msgs (title, category, 
+                             description, source, datetime)
+      VALUES('$title', $category, '$description', '$source',
+             $dt)";
+    return $this->_db->exec($sql);
+    
+  }
   function getNews(){}
   function deleteNews($id){}
+  function clearStr($data){
+    $data = strip_tags($data);
+    return $this->_db->escapeString($data);
+  }
+  function clearInt($data){
+    return abs((int)$data);
+  }
 }
 
