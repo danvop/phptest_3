76d175765c357c9c8fdceaa29ccd1937adb17dcf
danvop <danvop@gmail.com>
2015-07-09 23:04:55 +1000

    doom first tests

--- demo/xml/dom.php
+++ demo/xml/dom.php
@@ -1,5 +1,17 @@
 <?php 
   header( "Content-Type: text/html;charset=utf-8"); 
+
+  $dom = new DomDocument(); 
+  $dom->load("catalog.xml");
+  $root = $dom->documentElement;
+  //echo $root->nodeType;
+  //echo $root->textContent;
+  $books = $root->childNodes;
+  //var_dump($books);
+  foreach($books as $book){
+    echo $book->textContent;
+    echo "<hr>";
+  }
 ?>
 <html>
 
