f44687fec7ed861c19c9abdce3277da9289b04fd
danvop <danvop@gmail.com>
2015-08-02 22:16:15 +1000

    how to parse cbr.ru added

--- soap/soap-client.php
+++ soap/soap-client.php
@@ -14,3 +14,16 @@ try{
 }catch(SoapFault $e){
   echo 'Операция '.$e->faultcode.' вернула ошибку: '.$e->getMessage();
   }
+/* //example how to parse cbr.ru data
+$client = new SoapClient("http://www.cbr.ru/dailyinfowebserv/dailyinfo.asmx?wsdl");
+try{
+  //cbr.ru
+  $param["On_date"] = "2015-03-23T12:10:00";
+  $res = $client->GetCursOnDateXML($param);
+  $res->GetCursOnDateXMLResult->any;
+
+
+  var_dump($res);
+}catch(SoapFault $e){
+  echo 'Операция '.$e->faultcode.' вернула ошибку: '.$e->getMessage();
+  }*/
